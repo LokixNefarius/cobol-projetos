bf0f223bea5c7302993a1aeb41efb7b662f8d9f7
LokixNefarius <wesleymelo655@gmail.com>
2025-07-01 19:02:54 -0300

    funcionalidade criar conta adicionado

--- COBanko.cob
+++ COBanko.cob
@@ -1,25 +1,38 @@
+       *>-----------------IDENTIFICAÇÃO------------------
 	IDENTIFICATION DIVISION.
         PROGRAM-ID.    COBanko.
         AUTHOR.        WESLEY A. M.
-
+       *>-----------------AMBIENTE----------------------
         ENVIRONMENT DIVISION.
-
+       *>------------------DADOS-------------------------
         DATA DIVISION.
 
         WORKING-STORAGE SECTION.
-        01 OPCAO-MENU   PIC 9(1).
-
-        01 NUMERO-CONTA PIC 9(6).
-        01 NOME-CLIENTE PIC X(30).
-        01 SALDO           PIC S9(5)V99.
-
-        01 STATUS-CONTA       PIC X(7).
-            88 ATIVA     VALUE 'ATIVA'.
-            88 INATIVA   VALUE 'INATIVA'.
-
+       *>----------VARIÁVEIS------------
+        01 OPCAO-MENU            PIC 9(1).
+	01 NOME-CLIENTE          PIC X(30).
+	01 CONTADOR-CONTA        PIC 9(6) VALUE ZEROS.
+	01 DATA-COMPLETA         PIC X(20).
+       *>-------REGISTRO DE DADOS-------
+        01 REGISTRO-CONTA.
+	   05 NUMERO-CONTA PIC 9(6).
+	   05 DATA-CRIACAO.
+	      10 DD   PIC 9(2).
+	      10 MM   PIC 9(2).
+	      10 YYYY PIC 9(4).
+           05 NOME-CADASTRADO PIC X(30).
+	   05 SITUACAO  PIC X(7).
+           05 SALDO        PIC S9(5)V99.
+       
+        01 STATUS-CONTA PIC X(7).
+            88 ATIVA    VALUE 'ATIVA'.
+            88 INATIVA  VALUE 'INATIVA'.
+	
+       *>--------------------LÓGICA----------------------
         PROCEDURE DIVISION.
 	   PERFORM MENU-PRINCIPAL UNTIL OPCAO-MENU = 6
 	   STOP RUN.
+       *>------PARAGRAFO MENU--------
 	MENU-PRINCIPAL.
            DISPLAY "1 - CRIAR CONTA."
            DISPLAY "2 - CONSULTAR SALDO."
@@ -33,7 +46,7 @@
 
            EVALUATE OPCAO-MENU
               WHEN 1
-                 DISPLAY "CRIANDO CONTA..."
+                 PERFORM CRIAR-CONTA
               WHEN 2
                  DISPLAY "CONSULTANDO SALDO"
 	      WHEN 3
@@ -47,3 +60,23 @@
 	      WHEN OTHER
 	         DISPLAY "OPÇÃO INVALIDA."
            END-EVALUATE.
+       *>---PARAGRAFO CRIAR CONTA---
+	CRIAR-CONTA.
+	   DISPLAY "INSIRA O NOME DO CLIENTE"
+	   ACCEPT NOME-CLIENTE
+	   MOVE "ATIVA" TO SITUACAO
+	   PERFORM GERAR-NUMERO-CONTA
+	   MOVE NOME-CLIENTE TO NOME-CADASTRADO
+	   DISPLAY "CONTA CRIADA PARA " NOME-CLIENTE "COM STATUS " 
+							    SITUACAO
+           DISPLAY "DATA DE CRIAÇÃO: " DD "/" MM "/" YYYY.
+	   DISPLAY "SALDO: " SALDO.
+
+	GERAR-NUMERO-CONTA.
+	   ADD 1 TO CONTADOR-CONTA
+           DISPLAY "NÚMERO DA CONTA: " CONTADOR-CONTA
+	   MOVE FUNCTION CURRENT-DATE TO DATA-COMPLETA
+	   MOVE DATA-COMPLETA(7:2) TO DD
+	   MOVE DATA-COMPLETA(5:2) TO MM
+	   MOVE DATA-COMPLETA(1:4) TO YYYY.
+	   
